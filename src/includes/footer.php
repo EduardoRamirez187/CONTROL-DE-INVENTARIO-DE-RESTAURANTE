1621bfe165e3234c381ff34c77cc4dfe30327760
Eduardo RL <edualf55@gmail.com>
2024-09-30 15:57:42 -0600

    Eliminación de archivos innecesarios y modificaciones en footer.php

--- src/includes/footer.php
+++ src/includes/footer.php
@@ -5,10 +5,7 @@
 </div>
 <!-- Main Footer -->
 <footer class="main-footer">
-    <strong>Copyright &copy; 2023 <a href="#">Sistemas Free</a>.</strong>
-    All rights reserved.
-    <div class="float-right d-none d-sm-inline-block">
-        <b>Version</b> 3.2.0
+  
     </div>
 </footer>
 </div>
